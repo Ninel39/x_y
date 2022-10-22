508e27dab203ef42920ad44ec2c96390d34347d9
User <User@WIN-5U3F60ENH1L>
2022-10-22 23:29:18 +0300

    Добавьте файлы проекта.

---
+++ x_y/x_y.cpp
@@ -0,0 +1,26 @@
+﻿// x_y.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+
+	int x, y, sum = 0;
+	cout << "Введите число" << endl;
+	cin >> x;
+	cout << "Введите степень" << endl;
+    cin>> y;
+	if (x == 0 || y == 0)
+		cout << "Ошибка";
+	else
+	{
+		sum = pow(x, y);
+		cout << sum << endl;
+	}
+	
+	
+}
+
